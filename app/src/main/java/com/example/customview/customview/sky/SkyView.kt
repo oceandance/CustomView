343ef840e1028c8845c35c2874681d09c1b8f561
abdi_mukhit <mukhitabdinazarov@gmail.com>
2022-04-27 14:28:54 +0600

    draw stars

--- app/src/main/java/com/example/customview/customview/sky/SkyView.kt
+++ app/src/main/java/com/example/customview/customview/sky/SkyView.kt
@@ -4,22 +4,27 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Path
+import android.graphics.Point
 import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
+import java.util.*
 
 class SkyView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
 ) : View(context, attrs) {
 
-    private lateinit var skyPaint: Paint
-    private lateinit var moonPaint: Paint
+    private var skyPaint: Paint
+    private var moonPaint: Paint
+    private var starPaint: Paint
 
     private val skyRect: Rect = Rect()
 
-    override fun onAttachedToWindow() {
-        super.onAttachedToWindow()
+    private val starsPath: Path = Path()
+
+    init {
         skyPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             color = Color.BLUE
             alpha = 80
@@ -31,11 +36,17 @@ class SkyView @JvmOverloads constructor(
             color = Color.YELLOW
             style = Paint.Style.FILL
         }
+
+        starPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+            color = Color.YELLOW
+            style = Paint.Style.FILL
+        }
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
         skyRect.set(left, top, right, bottom/2)
+        drawStarsPath()
     }
 
     override fun onDraw(canvas: Canvas) {
@@ -43,6 +54,34 @@ class SkyView @JvmOverloads constructor(
 
         canvas.drawRect(skyRect, skyPaint)
         canvas.drawCircle(800f, 250f, 50f, moonPaint)
+        canvas.drawPath(starsPath, starPaint)
     }
 
+    private fun drawStarsPath() {
+        starsPath.fillType = Path.FillType.WINDING
+        for (i in 0..10) {
+            val starPixel = getRandomNumber(3, 10)
+            val starDiameter = starPixel * 6
+
+            val startX = getRandomNumber(0, width - starDiameter)
+            val startY = getRandomNumber(0, height / 2 - starDiameter)
+
+            val a = Point(startX, startY)
+            val b = Point(startX + starPixel * 2, startY + starPixel * 6)
+            val c = Point(startX - starPixel * 3, startY + starPixel * 2)
+            val d = Point(startX + starPixel * 3, startY + starPixel * 2)
+            val e = Point(startX - starPixel * 2, startY + starPixel * 6)
+
+            starsPath.moveTo(a.x.toFloat(), a.y.toFloat())
+            starsPath.lineTo(b.x.toFloat(), b.y.toFloat())
+            starsPath.lineTo(c.x.toFloat(), c.y.toFloat())
+            starsPath.lineTo(d.x.toFloat(), d.y.toFloat())
+            starsPath.lineTo(e.x.toFloat(), e.y.toFloat())
+            starsPath.lineTo(a.x.toFloat(), a.y.toFloat())
+        }
+    }
+
+    private fun getRandomNumber(min: Int, max: Int): Int {
+        return Random().nextInt(max - min + 1) + min
+    }
 }
